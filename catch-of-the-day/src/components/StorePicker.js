4c3accb3643ba94deef7fca4d736f306b24ae1e1
J Silverstein <jess.pebble@gmail.com>
2016-12-24 19:22:59 -0500

    commit after vid 4

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from 'react'; /// have to do this at the top of every component
+import { render } from 'react-dom';
+
+class StorePicker extends React.Component {
+  render() {
+    return <p>Hello</p>
+  }
+}
+
+export default StorePicker;
