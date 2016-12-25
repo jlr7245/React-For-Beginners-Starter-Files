0a326313b5bb8cbb0efbc7afc89ac4bec3430d53
J Silverstein <jess.pebble@gmail.com>
2016-12-24 19:35:53 -0500

    commit after vid 4

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,8 +3,18 @@ import { render } from 'react-dom';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello</p>
+    return (
+      <form className="store-selector">
+        { /* Here is a comment.... has to be inside yr html bc otherwise it's read as another top-level element. how disagreeable */ }
+        <h2>Please enter a store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
+      </form>
+      )
   }
 }
 
 export default StorePicker;
+
+
+/// vid 4: have to self-close tags.. can't do for ex `<br>`, has to be `<br/>`
