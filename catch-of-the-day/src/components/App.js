065e3f5766d174c272945df6ffa8e7ddcbe097dc
J Silverstein <jess.pebble@gmail.com>
2016-12-25 18:43:15 -0500

    commit after vid 7

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,10 +5,11 @@ import Inventory from './Inventory';
 
 class App extends React.Component { //// `Component` HAS TO BE UPPERCASE!!!!
   render() {
+    ///console.log(this);
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
         <Inventory />
@@ -18,3 +19,4 @@ class App extends React.Component { //// `Component` HAS TO BE UPPERCASE!!!!
 }
 
 export default App;
+// start by running `npm start` in the terminal
