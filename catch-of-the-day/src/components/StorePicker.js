17942323242a46070ee848a28f4bb22bf0860f4d
J Silverstein <jess.pebble@gmail.com>
2016-12-30 11:10:08 -0500

    commit after vid 12

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -11,8 +11,9 @@ class StorePicker extends React.Component {
     event.preventDefault();
     /// first grab the text from the box
     // second transition from / to /store/:storeId
-    console.log('you changed the URL');
-    console.log(this.storeInput.value)
+    /// "surface the router" using something called "context"
+    const storeId = this.storeInput.value;
+    this.context.router.transitionTo(`/store/${storeId}`);
   } /// relisten to vid 10 3 min in
   render() {
     return (
@@ -26,7 +27,20 @@ class StorePicker extends React.Component {
   }
 }
 
+
+/// just gotta know that you surface the router from the parent with a thing called context types. ok. thanks
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object,
+}
+
 export default StorePicker;
 
 
 /// vid 4: have to self-close tags.. can't do for ex `<br>`, has to be `<br/>`
+/* vid 12: props & state
+  - STATE: hold data 
+  - PROPS: pass data along to yr component
+  - BUT ALSO CONTEXT: declare at a top level & then it's available globally - while most of the time this isn't great bc React takes an individual component approach a la frank but the browserrouter is handy to be able to use
+
+*/
