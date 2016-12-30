f1f67cf61075ce721e5bf18c626502a031f0cccd
J Silverstein <jess.pebble@gmail.com>
2016-12-29 19:29:48 -0500

    commit after vid 11

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,12 +3,23 @@ import { getFunName } from '../helpers';
 import { render } from 'react-dom';
 
 class StorePicker extends React.Component {
+/*  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this); /// aw yis this thing i did for m'game
+  }*/
+  goToStore(event) {
+    event.preventDefault();
+    /// first grab the text from the box
+    // second transition from / to /store/:storeId
+    console.log('you changed the URL');
+    console.log(this.storeInput.value)
+  } /// relisten to vid 10 3 min in
   render() {
     return (
-      <form className="store-selector">
-        { /* Here is a comment.... has to be inside yr html bc otherwise it's read as another top-level element. how disagreeable */ }
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
+        { /* Here is a comment.... has to be inside yr html bc otherwise it's read as another top-level element. how unpleasant */ }
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={ (input) => {this.storeInput = input} }/>
         <button type="submit">Visit Store</button>
       </form>
       )
